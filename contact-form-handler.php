e99da5890356c0bb940d6f4e674b14a5f730cf08
Deida <deidalopez1@gmail.com>
2020-10-21 16:34:50 -0700

    updating folder

---
+++ contact-form-handler.php
@@ -0,0 +1,11 @@
+<?php
+    $name = $_POST['name']; 
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = "deidalopez7@gmail.com";
+    $email_subject = "New Message"; 
+    $email_body = "Name: $name.\n".
+                    "Email: $email.\n".
+                    "Message: $message.\n";
+
